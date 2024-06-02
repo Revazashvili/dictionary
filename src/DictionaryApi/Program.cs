af3e2cb59d4768623f65c4b8c7deca50fbd4adad
Revazashvili <levanrevazashvili@gmail.com>
2024-06-02 21:04:24 +0400

    change Program.cs class structure

--- src/DictionaryApi/Program.cs
+++ src/DictionaryApi/Program.cs
@@ -8,50 +8,68 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication()
-    .AddBearerToken(IdentityConstants.BearerScheme);
-builder.Services.AddAuthorizationBuilder();
-
-var connectionString = builder.Configuration.GetConnectionString("DictionaryDb");
-builder.Services.AddDbContext<DictionaryDbContext>(options => 
-    options.UseNpgsql(connectionString));
-
-builder.Services.AddIdentityCore<User>()
-    .AddEntityFrameworkStores<DictionaryDbContext>()
-    .AddApiEndpoints();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddScoped<ITopicService, TopicService>();
-builder.Services.AddScoped<IEntryService, EntryService>();
-
-builder.Services.ConfigureHttpJsonOptions(options =>
-{
-    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-});
-
-builder.Services.Configure<Microsoft.AspNetCore.Mvc.JsonOptions>(options =>
-{
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-});
+builder.Services.ConfigureServices(builder.Configuration);
 
 var app = builder.Build();
 
-var apiGroup = app.MapGroup("/api");
-apiGroup.MapCustomIdentityApi();
-apiGroup.MapTopicApi();
-apiGroup.MapSubTopicApi();
-
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
-
-var dictionaryDbContext = app.Services.CreateScope()
-    .ServiceProvider.GetRequiredService<DictionaryDbContext>();
-dictionaryDbContext.Database.Migrate();
+await app.Configure();
 
 app.Run();
+
+public static class WebApplicationExtensions
+{
+    public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication()
+            .AddBearerToken(IdentityConstants.BearerScheme);
+        services.AddAuthorizationBuilder();
+
+        var connectionString = configuration.GetConnectionString("DictionaryDb");
+        services.AddDbContext<DictionaryDbContext>(options => 
+            options.UseNpgsql(connectionString));
+
+        services.AddIdentityCore<User>()
+            .AddEntityFrameworkStores<DictionaryDbContext>()
+            .AddApiEndpoints();
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddScoped<ITopicService, TopicService>();
+        services.AddScoped<IEntryService, EntryService>();
+
+        services.ConfigureHttpJsonOptions(options =>
+        {
+            options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        });
+
+        services.Configure<Microsoft.AspNetCore.Mvc.JsonOptions>(options =>
+        {
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        });
+    }
+
+    public static async Task Configure(this WebApplication app)
+    {
+        var apiGroup = app.MapGroup("/api");
+        apiGroup.MapCustomIdentityApi();
+        apiGroup.MapTopicApi();
+        apiGroup.MapSubTopicApi();
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseHttpsRedirection();
+
+        await app.MigrateDatabase();
+    }
+
+    private static async Task MigrateDatabase(this WebApplication app)
+    {
+        var dictionaryDbContext = app.Services.CreateScope()
+            .ServiceProvider.GetRequiredService<DictionaryDbContext>();
+        await dictionaryDbContext.Database.MigrateAsync();
+    }
+}
